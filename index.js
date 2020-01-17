9d7319215365bb5fc38e2a1c0086495e3254eea1
brennnnan <brennan.mackay@sbcglobal.net>
2020-01-17 14:34:56 -0800

    added auto post to site

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const formidableMiddleware = require('express-formidable');
 var fs = require('fs');
 var path = require('path');
 var Chart = require('chart.js');
+var request = require('request');
 const getCountryISO3 = require("country-iso-2-to-3");
 var serveStatic = require('serve-static');
 
@@ -18,11 +19,26 @@ var filename = "";
 
 app.use(serveStatic('public', { 'index': ['index.html'] }))
 
+var options = {
+  'method': 'POST',
+  'url': 'https://jjdxb1aex5.execute-api.us-west-2.amazonaws.com/dev-2/soundexchange',
+  'headers': {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({"stream_channel":"JAZZGROOVE-WEST","year":"2019","month":"12","performanceFactor1":".135","performanceFactor2":".244"})
 
+};
 
 app.post('/', function (req, res){
-  console.log('Post Received.')
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+  });
 });
+
+
+
+
 
 
 
